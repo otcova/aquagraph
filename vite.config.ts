006f662bb095cd45779aba58007a2caa4829875c
otcova <otgercomas@gmail.com>
2023-04-01 12:36:20 +0200

    fix optimazedDeps

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import { defineConfig, optimizeDeps } from 'vite';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    optimizeDeps: {
-        exclude: [
-            "box2d-wasm",
-        ],
-    },
 });
